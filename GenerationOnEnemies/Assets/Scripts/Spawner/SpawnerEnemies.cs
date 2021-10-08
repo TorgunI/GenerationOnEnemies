4410e5621d7ce61143913ca88490eeffe416dc33
TorgunI <torgunivan@gmail.com>
2021-10-08 23:08:00 +0300

    Fix RequireComponent

--- GenerationOnEnemies/Assets/Scripts/Spawner/SpawnerEnemies.cs
+++ GenerationOnEnemies/Assets/Scripts/Spawner/SpawnerEnemies.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(GameObject))]
-
 public class SpawnerEnemies : MonoBehaviour
 {
     [SerializeField] private GameObject _enemyTemplate;
